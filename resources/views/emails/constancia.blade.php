7234a90f9071d80f9bd68d3c915ad83d107347c3
oamperez <oscar9913amperez@gmail.com>
2020-04-13 09:54:37 -0600

    Ajustes finales

--- resources/views/emails/constancia.blade.php
+++ resources/views/emails/constancia.blade.php
@@ -1,12 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>{{ $user['NOMBRE'] }}, te hemos enviado tu constancias.</h1>
-</body>
 </html>
+<style type="text/css" media="screen">
+
+    html, body {
+        height: auto;
+        width: 100%;
+        background: #FFF;
+        color: rgba(0,0,0,0.6);
+        font-family: verdana;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -o-user-select: none;
+        user-select: none;
+        padding: 1%
+    }
+    .container{
+        width: 50%;
+        height: auto;
+    }
+    .header{
+        background: #82C1E2;
+        height: 7%;
+        width: 100%;
+        padding: 0;
+        color: white;
+        text-align: center;
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+    }
+    .content{
+        background: #F8F8F8;
+        height: auto;
+        width: 100%;
+        padding: 0;
+    }
+    .body{
+        padding: 40px;
+    }
+    .parrafo{
+        padding: 0;
+        margin: 0;
+    }
+    .parrafo-interno{
+        padding: 0;
+        margin: 0;
+        padding-left: 20px;
+    }
+    h1.centrado{
+        padding: 18px;
+    }
+    </style>
+
+    <body>
+        <div class="container">
+            <div class="header">
+            </div>
+            <div  class="content">
+                <div class="body">
+                    <p>Ha recibido un documento de parte de operaciones de RH favor encontrarlo adjunto.</p>
+                </div>
+            </div>
+        </div>
+        <br>
+        Enviado desde FifcoOne.
+    </body>
